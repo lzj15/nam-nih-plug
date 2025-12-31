83cff76a65d004813ee33379dc7c24957b329b90
lzj15 <lzj15@proton.me>
2025-12-31 21:29:38 +0000

    Add three-band EQ

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
+use biquad::*;
 use nih_plug::prelude::*;
 use std::sync::Arc;
-mod nam;
+mod neuralaudio;
+
+const BASS_FREQ: f32 = 500.0;
+const MID_FREQ: f32 = 1500.0;
+const TREBLE_FREQ: f32 = 3000.0;
 
 struct Nam {
     params: Arc<NamParams>,
-    amp: Option<nam::Model>,
+    amp: Option<neuralaudio::Model>,
     temp_buffer: Vec<f32>,
+    filters: Option<[DirectForm1<f32>; 3]>,
 }
 
 /// The [`Params`] derive macro gathers all of the information needed for the wrapper to know about
@@ -18,8 +24,14 @@ struct NamParams {
     /// these IDs remain constant, you can rename and reorder these fields as you wish. The
     /// parameters are exposed to the host in the same order they were defined. In this case, this
     /// gain parameter is stored as linear gain while the values are displayed in decibels.
-    #[id = "volume"]
-    pub volume: FloatParam,
+    #[id = "bass"]
+    pub bass: FloatParam,
+    #[id = "mid"]
+    pub mid: FloatParam,
+    #[id = "treble"]
+    pub treble: FloatParam,
+    #[id = "output"]
+    pub output: FloatParam,
 }
 
 impl Default for Nam {
@@ -28,6 +40,7 @@ impl Default for Nam {
             params: Arc::new(NamParams::default()),
             amp: None,
             temp_buffer: Vec::new(),
+            filters: None,
         }
     }
 }
@@ -35,18 +48,48 @@ impl Default for Nam {
 impl Default for NamParams {
     fn default() -> Self {
         Self {
+            bass: FloatParam::new(
+                "Bass",
+                0.0,
+                FloatRange::Linear {
+                    min: -20.0,
+                    max: 20.0,
+                },
+            )
+            .with_unit(" dB"),
+
+            mid: FloatParam::new(
+                "Mid",
+                0.0,
+                FloatRange::Linear {
+                    min: -15.0,
+                    max: 15.0,
+                },
+            )
+            .with_unit(" dB"),
+
+            treble: FloatParam::new(
+                "Treble",
+                0.0,
+                FloatRange::Linear {
+                    min: -10.0,
+                    max: 10.0,
+                },
+            )
+            .with_unit(" dB"),
+
             // This gain is stored as linear gain. NIH-plug comes with useful conversion functions
             // to treat these kinds of parameters as if we were dealing with decibels. Storing this
             // as decibels is easier to work with, but requires a conversion for every sample.
-            volume: FloatParam::new(
-                "Volume",
+            output: FloatParam::new(
+                "Output",
                 util::db_to_gain(0.0),
                 FloatRange::Skewed {
-                    min: util::db_to_gain(-30.0),
-                    max: util::db_to_gain(30.0),
+                    min: util::db_to_gain(-20.0),
+                    max: util::db_to_gain(20.0),
                     // This makes the range appear as if it was linear when displaying the values as
                     // decibels
-                    factor: FloatRange::gain_skew_factor(-30.0, 30.0),
+                    factor: FloatRange::gain_skew_factor(-20.0, 20.0),
                 },
             )
             // Because the gain parameter is stored as linear gain instead of storing the value as
@@ -115,11 +158,41 @@ impl Plugin for Nam {
         self.temp_buffer = Vec::with_capacity(buffer_config.max_buffer_size as usize);
 
         self.amp = Some(
-            nam::Model::from_file(
+            neuralaudio::Model::from_file(
                 "/home/lzj/Data/music/NAM/[AMP] PRS-MT100 LEAD Noon SM57 - STD.nam",
             )
             .expect("Failed to load model"),
         );
+
+        self.filters = Some([
+            DirectForm1::<f32>::new(
+                Coefficients::<f32>::from_params(
+                    Type::LowShelf(0.0),
+                    buffer_config.sample_rate.hz(),
+                    BASS_FREQ.hz(),
+                    Q_BUTTERWORTH_F32,
+                )
+                .unwrap(),
+            ),
+            DirectForm1::<f32>::new(
+                Coefficients::<f32>::from_params(
+                    Type::PeakingEQ(0.0),
+                    buffer_config.sample_rate.hz(),
+                    MID_FREQ.hz(),
+                    Q_BUTTERWORTH_F32,
+                )
+                .unwrap(),
+            ),
+            DirectForm1::<f32>::new(
+                Coefficients::<f32>::from_params(
+                    Type::HighShelf(0.0),
+                    buffer_config.sample_rate.hz(),
+                    TREBLE_FREQ.hz(),
+                    Q_BUTTERWORTH_F32,
+                )
+                .unwrap(),
+            ),
+        ]);
         true
     }
 
@@ -127,20 +200,48 @@ impl Plugin for Nam {
         &mut self,
         buffer: &mut Buffer,
         _aux: &mut AuxiliaryBuffers,
-        _context: &mut impl ProcessContext<Self>,
+        context: &mut impl ProcessContext<Self>,
     ) -> ProcessStatus {
         self.temp_buffer = vec![0.0; buffer.samples()];
         self.temp_buffer
             .copy_from_slice(buffer.as_slice_immutable()[0]);
-
         self.amp
             .as_mut()
             .unwrap()
             .process(self.temp_buffer.as_slice(), buffer.as_slice()[0]);
 
+        let coeffs_bass = Coefficients::from_params(
+            Type::LowShelf(self.params.bass.value()),
+            context.transport().sample_rate.hz(),
+            BASS_FREQ.hz(),
+            Q_BUTTERWORTH_F32,
+        )
+        .unwrap();
+        let coeffs_mid = Coefficients::from_params(
+            Type::PeakingEQ(self.params.mid.value()),
+            context.transport().sample_rate.hz(),
+            MID_FREQ.hz(),
+            Q_BUTTERWORTH_F32,
+        )
+        .unwrap();
+        let coeffs_treble = Coefficients::from_params(
+            Type::HighShelf(self.params.treble.value()),
+            context.transport().sample_rate.hz(),
+            TREBLE_FREQ.hz(),
+            Q_BUTTERWORTH_F32,
+        )
+        .unwrap();
+
+        for (index, coeffs) in [coeffs_bass, coeffs_mid, coeffs_treble].iter().enumerate() {
+            self.filters.as_mut().unwrap()[index].update_coefficients(*coeffs);
+        }
+
         for sample in buffer.as_slice()[0].as_mut() {
+            for filter in self.filters.as_mut().unwrap() {
+                *sample = filter.run(*sample);
+            }
             // Smoothing is optionally built into the parameters themselves
-            *sample *= self.params.volume.smoothed.next();
+            *sample *= self.params.output.smoothed.next();
         }
 
         self.temp_buffer
